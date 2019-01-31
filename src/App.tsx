7aaf377bfb41e505ac302e06e55490024369223d
danh955 <16111574+danh955@users.noreply.github.com>
2019-01-30 17:12:31 -0800

    added React-Routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import './App.css';
 import TicTacToeGame from './tic-tac-toe/Game';
 
@@ -7,18 +8,59 @@ import logo from './logo.svg';
 class App extends React.Component {
   public render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <TicTacToeGame />
-      </div>
+      <Router>
+        <div className="App">
+          <header className="App-header">
+            <h1 className="App-title">Welcome to typescript-react1</h1>
+          </header>
+          <nav className="App-LeftNav">
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/about">About</Link>
+              </li>
+              <li>
+                <Link to="/Tic-Tac-Toe">Tic-Tac-Toe</Link>
+              </li>
+            </ul>
+          </nav>
+          <Route path="/" exact component={Home} />
+          <Route path="/about" component={About} />
+          <Route path="/Tic-Tac-Toe" component={TicTacToe} />
+        </div>
+      </Router>
     );
   }
+}
+
+function Home() {
+  return (
+    <section>
+      <img src={logo} className="App-logo" alt="logo" />
+      <h2>Home</h2>
+      here is some text in this little box.<br/>
+    </section>
+  );
+}
+
+function About() {
+  return (
+    <section>
+      <h2>About</h2>
+      and it's about time.
+    </section>
+  );
+}
+
+function TicTacToe() {
+  return (
+    <section>
+      <h2>Tic-Tac-Toe</h2>
+      <TicTacToeGame />
+    </section>
+  );
 }
 
 export default App;
